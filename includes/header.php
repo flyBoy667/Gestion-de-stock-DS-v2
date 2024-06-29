caa319503050cad45fbb7ace29546a68a8748fb0
flyBoy667 <salifoudembele100@outlook.com>
2024-06-29 18:59:52 +0000

    Optimisation de la navbar

--- includes/header.php
+++ includes/header.php
@@ -120,7 +120,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
             <!-- Sidebar user panel (optional) -->
             <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                 <div class="image">
-                    <img src="dist/img/admin.jpg" class="img-circle elevation-2" alt="User Image">
+                    <img src="dist/img/user.png" class="img-circle elevation-2" alt="User Image">
                 </div>
                 <div class="info">
                     <a href="#" class="d-block"><?= $_SESSION['nom'] . ' ' . $_SESSION['prenom']  ?></a>
